15f0ca3c94a5be78c1f6e7827261dce6608c42ae
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-06-27 16:47:44 +0200

    docs: add documentation for symbols

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,25 @@ import fs from "fs";
 import path from "path";
 import readline from "node:readline/promises";
 
+/**
+ * Arguments that can be passed to the script
+ */
 interface Arguments {
   [x: string]: unknown;
+  /**
+   * Optional path to the folder where the screenshots will be saved
+   * If not passed, will prompt the user to create a screenshots folder
+   */
   path: string | undefined;
+  /**
+   * URL to be screenshot
+   * If not passed, will default to `http://localhost:5173`
+   */
   url: string;
+  /**
+   * Show the browser window while taking screenshots
+   * If not passed, will default to `false`, eg. headless
+   */
   show: boolean;
 }
 
@@ -46,6 +61,9 @@ let screenshotPath = argv.path ?? "screenshots";
 let screenshotPathInProject = path.join(process.cwd(), screenshotPath);
 
 type Viewport = { width: number; height: number };
+/**
+ * Device with name and viewport size
+ */
 type Device = { name: string; viewport: Viewport };
 
 export const devices: Device[] = [
